2d75ccbbd5bec200effa752edb8b31d9e3676eca
kazukiomori <kazuki1110170501@gmail.com>
2025-06-22 20:17:10 +0900

    Initial Commit

---
+++ FitMate/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  FitMate
+//
+//  Created by Kazuki Omori on 2025/06/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ FitMate/FitMateApp.swift
@@ -0,0 +1,17 @@
+//
+//  FitMateApp.swift
+//  FitMate
+//
+//  Created by Kazuki Omori on 2025/06/22.
+//
+
+import SwiftUI
+
+@main
+struct FitMateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
